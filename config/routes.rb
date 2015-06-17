10b044e79882dd5bd66c214230416b976f043036
Travis <travis@myjewelology.com>
2015-06-17 01:31:22 -0400

    Added links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ root 'pages#home'
 
 get 'about' => "pages#about"
 
+devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
